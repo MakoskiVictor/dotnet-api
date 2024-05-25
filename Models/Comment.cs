f76d4eb9cadee8212e5e7947b24b941540dc9ced
Victor Makoski <vmakoski.training@innevo.cl>
2024-05-25 13:15:33 -0300

    add comments props

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -5,8 +5,13 @@ using System.Threading.Tasks;
 
 namespace aspnetwebapi.Models
 {
-    public class Comment
-    {
-        
-    }
+  public class Comment
+  {
+    // Navigation
+    public int? StockId { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedOn { get; set; } = DateTime.Now;
+    public Stock? Stock { get; set; }
+  }
 }
